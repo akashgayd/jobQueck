5edec1a2291fec2368e794ffb8bd0beb83eac7d0
Akash Gaydhane <117028938+akashgayd@users.noreply.github.com>
2025-01-25 20:43:35 +0530

    update method

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -20,4 +20,28 @@ const getProfiles = async (req, res) => {
     }
 };
 
-module.exports = { createProfile, getProfiles };
+
+// update the profile
+
+const updateProfile = async (req, res) => {
+
+    try{
+        const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(profile);
+
+        if(!profile){
+
+            return res.status(404).json({message: "Profile not found"});
+        }
+
+    }
+
+    
+    catch(err){
+        res.status(400).json({message: err.message});
+    }
+
+}
+
+
+module.exports = { createProfile, getProfiles , updateProfile};
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { createProfile, getProfiles } = require('../controllers/profileController');
+const { createProfile, getProfiles,updateProfile } = require('../controllers/profileController');
 const router = express.Router();
 
 router.post("/seed", createProfile);
 router.get("/", getProfiles);
+
+router.put('/update',updateProfile)
+
 
 module.exports = router;
