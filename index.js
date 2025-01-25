45e8c9b021a22df8f4a2286659bc5f7a4252153b
Akash Gaydhane <117028938+akashgayd@users.noreply.github.com>
2025-01-25 09:07:48 +0530

    commit2

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const cors = require("cors");
 const connectDB = require("./configdata/db");
 const narratorRoutes = require("./routes/narratorRoutes");
 const jobDecriptionRouter = require("./routes/jobDecriptionRouter");
-
 const contactUsRoutes = require('./routes/constctUsroutes');
+
+// Load environment variables
 dotenv.config();
 
+// Initialize Express app
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -20,9 +24,8 @@ app.use("/api/narrators", narratorRoutes);
 app.use("/api/jobdetails", jobDecriptionRouter);
 app.use("/api/contactus", contactUsRoutes);
 
-
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
